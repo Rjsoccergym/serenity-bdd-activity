87419074fba47c8448d5e15211c1331508b7bebf
hernanj.romerov <hernanj.romerov@autonoma.edu.co>
2024-09-28 08:23:46 -0500

    Actividad Serenity BDD

---
+++ buil d/test-results/timeline/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"d2e6df35-4934-4bf5-a5a3-8cbe4d40c319","feature":"Submit a form","scenario":"Submit a test form on Demoqa page","start":1727483513784,"group":1,"content":"","tags":"","end":1727483523471,"className":"failed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,Test worker,5,main]"}]);
+});
